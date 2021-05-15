4add136a56a33e52d145df3dfd35c61fe21f300e
iKotyaXR <iKotyaXR@yandex.ru>
2021-05-15 23:56:19 +0300

    new game

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-$(document).ready(function() {
-    $('.bata').on('click', () => {
-        $("#test").slideDown('slow')
-            .animate({ height: "+=200" }, 'slow')
-            .animate({ width: "+=200" }, 'slow')
-            .animate({ marginLeft: "+=100" }, 'slow')
-            .slideUp('slow')
-            .slideDown('slow')
-            .animate({ width: "-=200" }, 'slow')
-            .animate({ height: "-=200" }, 'slow')
-            .animate({ marginLeft: "-=100" }, 'slow')
-        return false;
-    })
+let sock = new WebSocket("ws://javascript.info");
+sock.onopen = function() {
+    console.log('open');
+    sock.send('test');
+};
 
-})
+sock.onmessage = function(e) {
+    console.log('message', e.data);
+    sock.close();
+};
+
+sock.onclose = function() {
+    console.log('close');
+};
